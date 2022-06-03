cff9795651a889d300a3b5633cae72f252783a97
FazwanUiTM <fzwnmd@gmail.com>
2022-06-04 00:51:00 +0800

    kk

---
+++ get_hint.php
@@ -0,0 +1,35 @@
+<?php
+// Array with names
+$a = ["EXORA","INSPIRA", "IRIZ", "PERDANA", " PERSONA", "PREVE",
+"SAGA", "SATRIA", "SUPRIMA S", "X50", "X70"];
+
+// get the q parameter from URL
+$q = $_REQUEST["q"];
+
+$hint = "";
+$option_hint = "";
+
+// lookup all hints from array if $q is different from ""
+if ($q !== "") {
+  $q = strtolower($q);
+  $len=strlen($q);
+  foreach($a as $name) {
+    if (stristr($q, substr($name, 0, $len))) {
+      if ($hint === "") {
+        $hint = $name;
+      } else {
+        $hint .= ", $name";
+      }
+      if ($option_hint === "") {
+        $option_hint = "<option>".$name."</option>";
+      } else {
+        $option_hint .= "<option>".$name."</option>";
+      }
+    }
+  }
+}
+
+// Output "no suggestion" if no hint was found or output correct values
+//echo $hint === "" ? "no suggestion" : $hint;
+echo $option_hint === "" ? "<option>no suggestion</option>" : $option_hint;
+?>
